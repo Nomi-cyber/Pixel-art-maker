1c183047e448250373b58774a38eb842afd3af1d
Numan ejaz <63910782+Nomi-cyber@users.noreply.github.com>
2020-04-22 10:20:18 +0300

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,37 @@
+let canvas = document.getElementById("pixel_canvas");
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById("colorPicker");
+
+color.addEventListener("click", function(){});
+
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+
+function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
+
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+// alternative code:
+// while (table.rows.length > 0) {
+//  table.deleteRow(0);
+// }
+
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
